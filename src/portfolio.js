85251f7d33f851aca820af807134b4bdf703b35d
larteyis <Lartey.isaac@gmail.com>
2021-12-08 22:00:31 -0500

    Updated git link

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://larteyis.github.io ',
+  homepage: 'https://larteyis.github.io',
   title: 'IL.',
 }
 
